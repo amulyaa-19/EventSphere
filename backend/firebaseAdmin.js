420705dbd871d13efed715105d84f3f35e786092
Amulya Srivastava <srivastava.amulya19@gmail.com>
2025-07-03 01:17:30 +0530

    added fireabase serv for render env

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,8 +1,15 @@
 const admin = require("firebase-admin");
 
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  serviceAccount = require("./firebase-service-account.json");
+}
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(require("./firebase-service-account.json")),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
